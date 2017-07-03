69ba2c9012e6622555e92353957c11aa2b35e3a6
Srijan Parmeshwar <srijan.parmeshwar@gmail.com>
2017-07-03 22:22:25 +0100

    Update settings for UCL jobs.

--- synthetic/scripts/run.py
+++ synthetic/scripts/run.py
@@ -3,11 +3,13 @@ import os, sys
 project_path = sys.argv[1]
 project_name = sys.argv[2]
 path = sys.argv[3]
+start = sys.argv[4]
+end = sys.argv[5]
 
 if project_name == 'suncg':
 	obj_path = project_path + "/obj"
 	ids = os.listdir(obj_path)
-	for id in ids:
-		os.system("blender-exp -b " + project_path + "/" + project_name + ".blend -P render.py -- " + path + " " + id + " suncg " + obj_path)
+	for id in ids[start:end]:
+		os.system("blender -b " + project_path + "/" + project_name + ".blend -P render.py -- " + path + " " + id + " suncg " + obj_path)
 else:
-	os.system("blender-exp -b " + project_path  + "/" + project_name + ".blend -P render.py -- " + path + " " + project_name + " blender")
+	os.system("blender -b " + project_path  + "/" + project_name + ".blend -P render.py -- " + path + " " + project_name + " blender")
