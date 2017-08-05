23f94d2c09b36d6fa8983a11cd33d5427d38e326
Srijan Parmeshwar <srijan.parmeshwar@gmail.com>
2017-08-05 13:05:22 +0100

    Fix name generation.

--- monodepth/utils/generate_names.py
+++ monodepth/utils/generate_names.py
@@ -21,17 +21,14 @@ all_filenames = []
 for path, subdirectories, filenames in os.walk(os.path.join(input_path, "top")):
     if not subdirectories:
         images = [filename for filename in filenames if filename.endswith(".jpg") or filename.endswith(".png")]
-        
+
         if os.path.isfile(os.path.join(path, "calibration.txt")):
             calibration = read_calibration(os.path.join(path, "calibration.txt"))
         else:
             calibration = "0 0 0"
         
         relative_path = os.path.relpath(path, os.path.join(input_path, "top"))
-        all_filenames.extend([os.path.splitext(os.path.join(relative_path, filename))[0] + " " + calibration for filename in images if os.path.isfile(os.path.join("bottom", path, filename))])
-
-if len(all_filenames) < 1:
-    exit(0)
+        all_filenames.extend([os.path.splitext(os.path.join(relative_path, filename))[0] + " " + calibration for filename in images if os.path.isfile(os.path.join(input_path, "bottom", relative_path, filename))])
 
 # Split into train and test sets.
 split_index = int(split_ratio * len(all_filenames))
@@ -39,10 +36,16 @@ train_filenames = all_filenames[:split_index]
 random.shuffle(train_filenames)
 test_filenames = all_filenames[(split_index + 1):]
 
+if len(train_filenames) < 1:
+    exit(0)
+
 # Write training filenames.
 with open(train_path, 'w') as train_file:
     train_file.write(train_filenames[0])
     train_file.writelines('\n' + filename for filename in train_filenames[1:])
+
+if len(test_filenames) < 1:
+    exit(0)
 
 # Write testing filenames.
 with open(test_path, 'w') as test_file:
